d3f74bb2db68c790645a1c3db38820163eac9346
Vince Roti <vince.roti@gmail.com>
2018-06-12 16:58:23 -0700

    first init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <p>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
